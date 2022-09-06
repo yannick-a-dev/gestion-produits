fe2238d45e0b88f14004fb8e8b171579a38dda86
Yannick ESSOLA <yannick.essola@conserto.pro>
2022-09-06 14:39:06 +0200

    first commit

---
+++ src/main/java/com/gestionproduits/gestionproduits/GestionProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.gestionproduits.gestionproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionproduits/gestionproduits/GestionProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionproduits.gestionproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
